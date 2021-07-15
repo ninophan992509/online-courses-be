985861b0c447ca62fc7bd18da084a1ce64ea1cbb
Toàn Lê <lmtoan99@gmail.com>
2021-07-15 16:11:01 +0700

    update lesson model

--- models/lesson.js
+++ models/lesson.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      lesson.hasMany(models.video,{foreignKey: 'lessonId'});
+      lesson.hasOne(models.video,{foreignKey: 'lessonId'});
       lesson.hasMany(models.document,{foreignKey: 'lessonId'})
     }
   };
